8323b50f9ca5a1878ea933ec84eb6aebd1e9cb0c
Aanya Shukla <aanyashukla67@gmail.com>
2024-11-10 11:33:58 +0530

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,39 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "😇️",
+    name: "Smiling Face With Halo",
+    meaning:
+      "A heavenly smile is shown by the halo emoji, which symbolizes innocence and goodness. People use it to express their good deeds, loving behavior or exemplary attitude.",
+  },
+  {
+    id: 5,
+    emoji: "🧐️",
+    name: "Face With Monocle",
+    meaning:
+      "It shows furrowed eyebrows and a slightly raised head, as if examining something closely. Used to indicate that something seems suspicious to you, or that you are making a skeptical or ironic observation. ",
+  },
+];
+
+export default emojipedia;
